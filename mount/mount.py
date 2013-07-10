7163f9f7b74a4fece89511ed36ed9cd6b600fcb8
rahulc93 <rahulc93@gmail.com>
2013-07-11 00:53:02 +0530

    Added the file mount.py

--- mount/mount.py
+++ mount/mount.py
@@ -2,3 +2,4 @@
 fl = open("/proc/mounts") # open the file /proc/mount
 for line in fl: # loop through the lines in the file
     print line, # print a line from the file in each loop
+    fl.close() # close the file /proc/mounts
