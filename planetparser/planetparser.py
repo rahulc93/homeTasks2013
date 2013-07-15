451aff28b4bb62be04683a96ec76f177539e986d
Rahul Chowdhury <rahul@rahul.Laptop>
2013-07-16 02:08:39 +0530

    Added comments to the file planetparser.py

--- planetparser/planetparser.py
+++ planetparser/planetparser.py
@@ -3,18 +3,18 @@
 import urllib2
 from bs4 import BeautifulSoup
 
-blog_url = "http://planet.fedoraproject.org/"
-url_open = urllib2.urlopen(blog_url)
-html_source = url_open.read()
-soup = BeautifulSoup(html_source)
-title_list = soup.find_all("div", class_="blog-entry-title")
-author_list = soup.find_all("div", class_="blog-entry-author")
-for i in range(len(title_list)):
-    soup_title = BeautifulSoup(str(title_list[i]))
-    title = soup_title.get_text()
-    print "Title: ", title
-    soup_author = BeautifulSoup(str(author_list[i]))
-    author = soup_author.div.a.get_text()
-    print "Author: ", author
-    print ""
+blog_url = "http://planet.fedoraproject.org/"  # the link from which to read the blog posts and authors
+url_open = urllib2.urlopen(blog_url)  # opening the URL of the blog
+html_source = url_open.read()  # reading the html source code of the blog page and storing it into 'html_source'
+soup = BeautifulSoup(html_source)  # passing "html_doc" into the BeautifulSoup constructor
+title_list = soup.find_all("div", class_="blog-entry-title")  # extracting a list of all the titles from the blog
+author_list = soup.find_all("div", class_="blog-entry-author")  # extracting a list of all the authors to the blog posts from the blog
+for i in range(len(title_list)):  # iterating through the length of title_list
+    soup_title = BeautifulSoup(str(title_list[i]))  # passing the i-th element of title_list to BeautifulSoup costructor
+    title = soup_title.get_text()  # storing the title of the post in 'title'
+    print "Title: ", title  # printing the title of the post
+    soup_author = BeautifulSoup(str(author_list[i]))  # passing the i-th element of author_list to BeautifulSoup constructor
+    author = soup_author.div.a.get_text()  # storing the author's name in 'author'
+    print "Author: ", author  # printing the author's name
+    print ""  # printing a newline
 
